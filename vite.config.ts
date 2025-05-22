39492883a6a4452a59e9c12023d69903a567bb8e
777iiddi <135117340+777iiddi@users.noreply.github.com>
2025-05-22 23:59:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { viteStaticCopy } from "vite-plugin-static-copy"; // ✅ Ajout du plugin
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,15 +11,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "staticwebapp.config.json",
-          dest: ".", // Copie à la racine du dossier dist
-        },
-      ],
-    }),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
